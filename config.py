e1923141093596d835b401a36f84f3e2a368750e
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-04-28 19:50:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", 12345))
+API_HASH = os.environ.get("API_HASH")
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+DB_URI = os.environ.get("DATABASE_URL", "")
