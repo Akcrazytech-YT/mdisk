054c4de21915863a8277f80e2bf8ebf77d22f15a
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-04-28 19:53:07 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -10,7 +10,7 @@ import asyncio
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
 
 
-from bot.config import Config
+from config import Config
 
 
 def start() -> scoped_session:
