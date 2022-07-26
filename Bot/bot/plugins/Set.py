039c490782ed6f469681706c3935e39026c9840a
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-07-26 17:07:09 +0530

    Update Set.py

--- Bot/bot/plugins/Set.py
+++ Bot/bot/plugins/Set.py
@@ -1,31 +1,43 @@
-import os
+import logging
+import requests
+from random import random
+import secrets
 import time
-import random
-from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from Bot.bot import Bot
 from Bot.vars import Var
+from urllib.parse import quote_plus
+import traceback
+import asyncio
+import aiohttp
+import string
+import io
+import os
+import shutil
+import sys
 
 @Bot.on_message(filters.private & filters.text)
-async def download_handler(client, message):
-    valid_url=[]
-    text=message.text.split()
-    reply_text="💥TODAY BEST NEW VIDEOS 👇\n\n"
-    for url in text:
-        if url_validator(url):
-            valid_url.append(url)
-    for x in range(len(valid_url)):
-        reply_text=reply_text + f"VIDEO {str(x+1)})\n{valid_url[x]}\n\n"
-    reply_text=reply_text+"━━━━━━━━━━━━━━━\n\nHow To Watch Tutorial 👇\n\nhttps://t.me/open_streaam/14"
-    await message.reply_text(reply_text, parse_mode="markdown", disable_web_page_preview=True)        
-
-# https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
-
+async def mdisk_convertor(client, message):
+  url=[]
+  text=message.text.split()
+  id=url.split('/')[5]
+  resp=requests.get(f'https://diskuploader.entertainvideo.com/v1/file/cdnurl?param={id}')
+  response=resp.json
+  source=response ['source']
+  name=response ['filename']
+  msg=f"Mdisk Link Converted\nFile Name : {name}\n\n mpd : `{source}`"
+  for url in text:
+    if url_validator(url):
+      await message.reply_text(msg, parse_mode="markdown", disable_web_page_preview=True)
+      else:
+        await message.reply_text("Send Valid Url")
+        
+        
 def url_validator(url):
-    URLRegex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return bool(re.match(URLRegex, url))
+  URLRegex = re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+  return bool(re.match(URLRegex, url))
